5c08526e427ac80ad20bdfc437ebc244cc8c127a
Anton Putra <me@antonputra.com>
2023-02-17 12:29:28 -0800

    asd

--- lessons/154/README.md
+++ lessons/154/README.md
@@ -13,3 +13,13 @@ kubectl apply --server-side -f prometheus-operator/crds
 kubectl apply -f prometheus-operator/rbac/cluster-roles.yaml
 kubectl apply -f prometheus-operator/namespace.yaml
 kubectl apply -f prometheus-operator/deployment
+kubectl get pods -n monitoring
+kubectl logs -l app.kubernetes.io/name=prometheus-operator -n monitoring -f
+
+2. Deploy Prometheus
+kubectl apply -f prometheus
+kubectl get pods -n monitoring
+kubectl logs -l app.kubernetes.io/name=prometheus -n monitoring -f
+
+
+3. Deploy Sample App
--- lessons/154/myapp/main.go
+++ lessons/154/myapp/main.go
@@ -6,6 +6,7 @@ import (
 	"net/http"
 	"time"
 
+	"github.com/gofiber/fiber/v2"
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
@@ -21,14 +22,16 @@ func NewMetrics(reg prometheus.Registerer) *metrics {
 			Name:       "duration_seconds",
 			Help:       "Duration of the request.",
 			Objectives: map[float64]float64{0.9: 0.01, 0.99: 0.001},
-		}, []string{"db"}),
+		}, []string{"path", "status"}),
 	}
 	reg.MustRegister(m.duration)
 	return m
 }
 
-func init() {
-	rand.Seed(time.Now().UnixNano())
+type Device struct {
+	ID       int    `json:"id"`
+	Mac      string `json:"mac"`
+	Firmware string `json:"firmware"`
 }
 
 func main() {
@@ -42,4 +45,37 @@ func main() {
 	go func() {
 		log.Fatal(http.ListenAndServe(":8081", pMux))
 	}()
+
+	go simulateTraffic(m)
+
+	app := fiber.New()
+
+	app.Get("/api/devices", getDevices)
+
+	log.Fatal(app.Listen(":8080"))
+}
+
+func getDevices(c *fiber.Ctx) error {
+	sleep(1000)
+	dvs := []Device{
+		{1, "5F-33-CC-1F-43-82", "2.1.6"},
+		{2, "EF-2B-C4-F5-D6-34", "2.1.6"},
+	}
+
+	return c.JSON(dvs)
+}
+
+func simulateTraffic(m *metrics) {
+	for {
+		now := time.Now()
+		sleep(1000)
+		m.duration.WithLabelValues("/fake", "200").Observe(time.Since(now).Seconds())
+	}
+}
+
+func sleep(ms int) {
+	rand.Seed(time.Now().UnixNano())
+	now := time.Now()
+	n := rand.Intn(ms + now.Second())
+	time.Sleep(time.Duration(n) * time.Millisecond)
 }
